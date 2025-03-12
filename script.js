dd255170bab094a9f09582c591260d244db52799
hahahalove57 <148331696+hahahalove57@users.noreply.github.com>
2025-03-12 20:22:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function showMessage() {
-    alert("仍在建设中，敬请等待！");
-}
+document.getElementById("blogButton").addEventListener("click", function() {
+    window.location.href = "lab3.html"; // 这里改成你的实验报告页面
+});
